ad38b68627bb2ea702c5c3119ceac7ffc1e80cbf
LA Bauserman <2512380+labauserman@users.noreply.github.com>
2026-02-07 12:13:09 -0500

    Fix type. error in test_types.py

--- test_types.py
+++ test_types.py
@@ -2,7 +2,7 @@ def add_numbers(a: int, b: int) -> int:
     return a + b
 
 # This should show an error - passing string to int parameter
-result = add_numbers("hello", 5)
+result = add_numbers(4, 5)
 
 # This should show an error - wrong type assignment
 x: int = "not a number"
